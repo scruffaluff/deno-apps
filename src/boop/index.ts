89887de45ea73577815c9788f4b51c5488c864c3
Macklan Weinstein <macklan.weinstein@gmail.com>
2025-02-14 01:23:35 -0800

    Add a webview callback

--- src/boop/index.ts
+++ src/boop/index.ts
@@ -4,9 +4,13 @@ import * as path from "jsr:@std/path";
 import { Webview } from "jsr:@webview/webview@0.9.0";
 
 const html = await Deno.readTextFile(
-  path.join(import.meta.dirname, "index.html")
+  path.join(import.meta.dirname!, "index.html")
 );
 
 const webview = new Webview();
+webview.title = "Boop";
+webview.bind("sendMessage", async (message: string) => {
+  await Deno.writeTextFile("server.log", message, { append: true });
+});
 webview.navigate(`data:text/html,${encodeURIComponent(html)}`);
 webview.run();
